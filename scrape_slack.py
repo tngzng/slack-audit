137ecb43715132686a12b1a28acbcb6ed54784d1
ting zhang <thetingzhang@gmail.com>
2021-02-25 12:27:20 -0500

    [Change] log level and improve logging

--- scrape_slack.py
+++ scrape_slack.py
@@ -28,7 +28,7 @@ MSG_TS_COL = "message_ts"
 
 
 def get_channel_id(slack: WebClient, channel_name: str) -> str:
-    logging.info(f"Fetching channel_id for channel {channel_name}...")
+    logging.debug(f"Fetching channel_id for channel {channel_name}...")
     response = slack.conversations_list(types="public_channel, private_channel")
     channels = response["channels"]
     channel_id = next(
@@ -38,13 +38,13 @@ def get_channel_id(slack: WebClient, channel_name: str) -> str:
 
 
 def get_all_members(slack: WebClient, channel_id: str) -> List[str]:
-    logging.info(f"Fetching users in channel_id {channel_id}...")
+    logging.debug(f"Fetching users in channel_id {channel_id}...")
     response = slack.conversations_members(channel=channel_id)
     return response["members"]
 
 
 def get_user_data(slack: WebClient, user_ids: List[str]) -> pd.DataFrame:
-    logging.info(f"Fetching user data for {len(user_ids)} users...")
+    logging.debug(f"Fetching user data for {len(user_ids)} users...")
     user_df = pd.DataFrame(columns=[USER_NAME_COL, USER_ID_COL, USER_EMAIL_COL])
     for user_id in user_ids:
         response = slack.users_profile_get(user=user_id)
@@ -64,7 +64,7 @@ def get_user_data(slack: WebClient, user_ids: List[str]) -> pd.DataFrame:
 def get_channel_messages(
     slack: WebClient, channel_id: str, days_to_fetch: int
 ) -> pd.DataFrame:
-    logging.info(f"Fetching messages for channel_id {channel_id}...")
+    logging.debug(f"Fetching messages for channel_id {channel_id}...")
     LIMIT = 200
     message_df = pd.DataFrame(
         columns=[MSG_TYPE_COL, USER_ID_COL, MSG_TEXT_COL, MSG_TS_COL]
@@ -77,7 +77,6 @@ def get_channel_messages(
             channel=channel_id, oldest=start_ts, limit=LIMIT, cursor=cursor
         )
         messages = response["messages"]
-        logging.info(f"FETCHED {len(messages)} messages")
         for message in messages:
             # bot messages will not have a user
             if not message.get("user"):
@@ -101,22 +100,24 @@ def get_channel_messages(
     return message_df
 
 
-def run_audit(channel_name: str, slack_token: str, days_to_fetch: int) -> None:
-    logging.info("Starting audit...")
+def scrape_messages(channel_name: str, slack_token: str, days_to_fetch: int) -> None:
+    logging.info("Fetching Slack messages...")
     slack = WebClient(token=slack_token)
     channel_id = get_channel_id(slack, channel_name)
     if not channel_id:
-        logging.info(f"No channel found matching name: {channel_name}")
+        logging.error(f"No channel found matching name: {channel_name}")
         return
     user_ids = get_all_members(slack, channel_id)
     user_df = get_user_data(slack, user_ids)
     message_df = get_channel_messages(slack, channel_id, days_to_fetch)
     message_df = message_df.merge(user_df, on=USER_ID_COL)
-    output_path = f"/app/outputs/{channel_name}.csv"
-    message_df.to_csv(output_path)
+    relative_path = f"outputs/{channel_name}.csv"
+    message_df.to_csv(f"/app/{relative_path}")
+    logging.info(f"Fetch complete. Output saved to: '{relative_path}'")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
     ap = argparse.ArgumentParser()
     ap.add_argument("-c", "--channel", required=True, help="Channel to audit")
     ap.add_argument(
@@ -137,4 +138,4 @@ if __name__ == "__main__":
     channel = args["channel"]
     token = args["token"]
     days_to_fetch = args["days"]
-    run_audit(channel, token, days_to_fetch)
+    scrape_messages(channel, token, days_to_fetch)
